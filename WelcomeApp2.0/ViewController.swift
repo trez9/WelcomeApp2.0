744ae6e71f362c97e8e40a52f06fb4835fb7d598
Tara Rezvani <tararezvani@Tara-Rezvanis-MacBook-Pro.local>
2015-01-02 20:07:16 -0800

    initial

---
+++ WelcomeApp2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WelcomeApp2.0
+//
+//  Created by Tara Rezvani on 1/2/15.
+//  Copyright (c) 2015 Tara Rezvani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
